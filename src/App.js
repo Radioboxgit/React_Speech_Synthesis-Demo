3bf151cc5d69d41cc8235c39dbdfd52f11516587
Radioboxgit <chinedulawrence43@yahoo.com>
2022-05-27 13:48:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Banner from './Banner.js'
+import styles from './Style.module.css';
+import Footer from "./Footer"
+
+
 
 function App() {
+  const limit=parseInt(10000);
+  const placeholder=`Text should not be more than ${limit} characters. 
+  Every fullstop should be preceded with a space before the next sentence begins.`
+    const [error,setError]=useState('')
+    const [text,setText] =useState('');
+
+  
+  
+   const transcribeText= async() =>{
+     setError("")
+     if (!text){
+       setError('No text entered')
+       return
+     }
+        try{
+        const utt=new SpeechSynthesisUtterance(text);
+        utt.volume=1
+        utt.rate=1.2
+        utt.pitch=8
+        speechSynthesis.speak(utt)
+    } catch(error){
+      setError(error.message) }
+    }
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner />
+      <div className={styles.container}>
+        <h3 className={styles.heading}>React Speech Synthesis Demo</h3>
+        <div className={styles.form}>
+          <textarea placeholder={placeholder}  maxLength={limit} className={styles.textarea} onChange={(e) => {setText(e.target.value)}} rows="10" cols="50"></textarea>
+          <p className={styles.indicator}>({text.length})/{limit} Characters</p>
+       </div>
+      <div className={styles.control}><button onClick={transcribeText}>Read Aloud</button></div>
+    {error &&  <p className={styles.error}>ERROR: {error} </p>}
+    </div>  
+    <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check for title React Demo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("React speech Synthesis Demo");
   expect(linkElement).toBeInTheDocument();
 });
